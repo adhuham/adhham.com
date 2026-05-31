2d12a20f80a7c3259bd780f2ada9507b08c606d2
Mohamed Adhuham <hey@adhham.com>
2026-05-31 23:28:53 +0500

    add clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from 'next'
 import { Inter, Manrope } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({
@@ -45,9 +46,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={inter.variable}>
+      <head>
+        {process.env.NODE_ENV === 'production' && 
+          <Script
+            id="clarity"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "wzu1hiemn6");
+              `,
+            }}
+          />
+        }
+      </head>
       <body className={`${inter.className} font-sans antialiased text-foreground`}>
         {children}
-        {process.env.NODE_ENV === 'production' && <Analytics />}
+        {/*process.env.NODE_ENV === 'production' && <Analytics />*/}
       </body>
     </html>
   )
